d442c6bfb4b906d9408f60c9058d54000c4a3c54
= <deluongo@gmail.com>
2016-12-07 23:03:32 -0500

    Debug db'

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'unix_socket' => env('DB_UNIX_SOCKET', ''),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
