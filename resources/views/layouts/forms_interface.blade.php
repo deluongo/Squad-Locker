c5850381ad8922ba40ab9871802910f752f3ddb0
Devon Luongo <deluongo@gmail.com>
2016-12-08 21:59:35 -0800

    Design Update for 99cont

--- resources/views/layouts/forms_interface.blade.php
+++ resources/views/layouts/forms_interface.blade.php
@@ -241,12 +241,12 @@
         <!-- Sidebar -->
         <nav id="sidebar">
             <!-- Sidebar Scroll Container -->
-            <div id="sidebar-scroll">
+            <div id="sidebar-scroll" class="bg-yellow-theme">
                 <!-- Sidebar Content -->
                 <!-- Adding .sidebar-mini-hide to an element will hide it when the sidebar is in mini mode -->
                 <div class="sidebar-content">
                     <!-- Side Header -->
-                    <div class="side-header side-content bg-white-op">
+                    <div class="side-header side-content bg-image" style="background-image: url({{ asset('/img/yellow.png') }})">
                         <!-- Layout API, functionality initialized in App() -> uiLayoutApi() -->
                         <button class="btn btn-link text-gray pull-right hidden-md hidden-lg" type="button" data-toggle="layout" data-action="sidebar_close">
                             <i class="fa fa-times"></i>
@@ -289,10 +289,6 @@
                                 </li>
                             </ul>
                         </div>
-                        <a class="h5 text-white" href="/">
-                            <span class="h4 font-w600 sidebar-mini-hide">Squad L</span><i class="fa fa-circle-o-notch text-primary"></i>
-                            <span class="h4 font-w600 sidebar-mini-hide">cker</span>
-                        </a>
                     </div>
                     <!-- END Side Header -->
 
